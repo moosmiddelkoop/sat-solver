ba4dfe050bf862668768d21f4d794534ff9b2589
Moos <jimmemiddelkoop@gmail.com>
2022-11-11 00:26:53 +0100

    Created SudokuSolver class, implemented read_input and add_rules methods

--- SAT.py
+++ SAT.py
@@ -1,27 +1,64 @@
 import sys
 import math
+import numpy as np
 
-def sudoku_to_dimacs(sudoku):
+class SudokuSolver:
 
-    clauses = []
-    nr_clauses = 0
-    nr_literals = math.sqrt(len(sudoku))
+    def __init__(self, input, rules):
+        self.input = input
+        self.literal_arr = None
+        self.size = math.sqrt(len(self.input[0]))
+        self.nr_literals = self.size ** 3
+        self.unit_clauses = []
+        self.rules = rules
+        self.clauses = None
 
-    for i, char in enumerate(sudoku):
-        if char == ".":
-            continue
-        else:
-            digit = char
-            row = i // 4
-            column = i % 4
-            clauses.append(f"{digit}{row}{column}")
-            nr_clauses += 1
 
-    print(f"p cnf {nr_clauses} {nr_literals}")
-    for clause in clauses:
-        print(clause + " 0")
+    def read_input(self) -> None:
 
-    return
+        # initialize the literal spoce
+        self.literal_arr = np.zeros((self.size, self.size, self.size))
+
+        for i, char in enumerate(self.input):
+
+            if char == ".":
+                continue
+            else:
+                digit = char
+                row = i // self.size
+                column = i % self.size
+                self.unit_clauses.append(f"{digit}{row}{column}")
+
+            # set unit clauses to true
+            self.literal_arr[row, column, digit] == 1
+
+
+    def add_rules(self) -> None:
+
+        ## PART1: turn rules file into list of clauses
+        rules_cnf = []
+
+        # turn rules txt file into list of clauses. Loop over all but the first line
+        with open(self.rules, 'r') as f:
+            for clause in f[1:]:
+
+                # add clauses as list of variables to list of clauses. Ignore last element bc this is 0 for each line
+                split_clause = clause.split[" "]
+                rules_cnf.append(split_clause[:-1])
+
+        ## PART2: append rules clauses to unit clauses
+        self.clauses = self.unit_clauses + rules_cnf
+
+
+    def DPLL(self):
+        '''Uses literal 3d array and clauses list'''
+        pass
+
+                
+    def run(self) -> None:
+        self.read_input()
+        self.add_rules()
+
 
 if __name__ == "__main__":
 
@@ -31,5 +68,16 @@ if __name__ == "__main__":
     strategy = sys.argv[1]
     input_file = sys.argv[2]
 
-    sudoku_to_dimacs(input_file)
+    # fetch the correct rules file
+    if len(input_file[0]) == 16:
+        rules_file = "rules/sudoku-rules-4x4.txt"
+    elif len(input_file[0]) == 81:
+        rules_file = "rules/sudoku-rules-9x9.txt"
+    elif len(input_file[0]) == 256:
+        rules_file = "rules/sudoku-rules-16x16.txt"
+    else:
+        raise Exception("Unexpected sudoku size. Supported sudoku sizes are: 4x4, 9x9, 16x16")
+
+    solution_object = SudokuSolver(input_file, rules_file)
+    solution_object.run()
 
