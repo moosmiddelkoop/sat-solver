c0e349f55f3876016002a3f43bde3b7288d8f5a5
Moos <jimmemiddelkoop@gmail.com>
2022-11-14 13:55:50 +0100

    Function updates
    Co-authored-by: Max Emanuel Feucht <MaxFeucht@users.noreply.github.com>
    Co-authored-by: AlexiaSalomons <AlexiaSalomons@users.noreply.github.com>

--- dpll.py
+++ dpll.py
@@ -1,22 +1,65 @@
 
-test_input = [[1, 2, 3], [1, -2], [1, -3], [-2, 3]]
+test_clauses = [[1, 2, 3], [1, -2], [1, -3], [-2, 3]]
 literals = [1, 2, 3]
 
+def get_vars(clauses):
+    '''
+    input: 2D array of clauses
+    output: dictionary of variables with values none
+
+    WORKS
+    '''
+
+    vars = dict()
+    for clause in clauses:
+        for var in clause:
+            vars[var] = None
+
+    return vars
 
 found_literals = []
+
+
+def simplify_clauses(clauses, literal):
+    """
+    XXXX
+    """
+    
+    for clause in clauses:
+
+        if literal in clause:
+                clauses.remove(clause)
+            
+        elif -literal in clause:
+                clause.remove(literal)
+                
+    return clauses
+
+def check_result(clauses):
+    """
+    XXXX
+    """
+    
+    # SAT: No v
+    if len(clauses) == 0:
+        found_literals.append(literal)
+        return True
+
+    # contradiction/UNSAT
+    elif len([clause for clause in clauses if len(clause) == 0]) >= 1:
+        found_literals.remove(literal)
+        return False
+
 
 def solver2(clauses, literal):
 
     global found_literals
     temp_clauses = clauses.deepcopy()
 
-    for clause in temp_clauses:
+    # First: for literal == FALSE
+    # PUT TRUTH ASSIGNMENT (FALSE) HERE
+    temp_clauses = simplify_clauses(temp_clauses, literal)
 
-        if literal in clause:
-                temp_clauses.remove(clause)
-            
-        elif -literal in clause:
-                clause.remove(literal)
 
     # SAT
     if len(temp_clauses) == 0:
@@ -40,6 +83,7 @@ def solver2(clauses, literal):
         return solver2(temp_clauses, -new_literal)
 
 
-solver2()
+# solver2()
 
-
+# get_vars test
+print(get_vars(test_clauses))
