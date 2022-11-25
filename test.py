ca5e7b52e74b9b51491653d436192175cd2f2b91
Moos <jimmemiddelkoop@gmail.com>
2022-11-25 15:21:53 +0100

    expanded

--- test.py
+++ test.py
@@ -1,23 +1,61 @@
 from SAT import *
+import matplotlib.pyplot as plt
+import numpy as np
+import time
 
-clauses = read_DIMACS(sys.argv[1])
-HEURISTIC = None
+# argument handling
+heuristic = sys.argv[1]
+clauses = read_DIMACS(sys.argv[2])
+output_path = sys.argv[3]
 
-solution = dpll(clauses, HEURISTIC)
+start = time.time()
 
-satisfiability = (solution[0])
-variables = (solution[1])
-backtracks = (solution[2])
+solution = dpll(clauses, heuristic)
+
+end = time.time()
+
+satisfiability = (solution[0][0])
+variables = (solution[0][1])
+backtracks = (solution[1])
+assignments = (solution[2])
+depths = (solution[3])
+speed = (end - start)
 
 print(f"Satisfiability: {satisfiability}")
 
 see_backtracks = input("Do you want to see the number of backtracks? (y/n): ")
-
 if see_backtracks == 'y':
     print(backtracks)
 
-see_vars = input("Do you want to see the variables? (y/n): ")
+see_assignments = input("Do you want to see the number of assignments? (y/n): ")
+if see_assignments == 'y':
+    print(assignments)
 
+see_vars = input("Do you want to see the variables? (y/n): ")
 if see_vars == 'y':
     print(variables)
 
+x_axis = np.linspace(0, len(depths), len(depths))
+y_axis = depths
+
+with open(output_path, 'w') as f:
+    f.write('BACKTRACKS\n')
+    f.write(f'{str(backtracks)}\n')
+
+    f.write('\nSPEED IN SECONDS\n')
+    f.write(f'{str(speed)}\n')
+
+    f.write('\nASSIGNMENTS\n')
+    f.write(f'{str(assignments)}\n')
+
+    f.write('\nDEPTHS\n')
+    for depth in depths:
+        f.write(f'{str(depth)}\n')
+
+
+# quickly plot depths
+            
+plt.title(f'Plot of the depth of the search tree over time. heuristic = {heuristic}')
+plt.plot(x_axis, y_axis)
+plt.show()
+plt.savefig(f'figs/depths/sudoku5_depths.png')
